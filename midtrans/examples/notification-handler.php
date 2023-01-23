36d303e4a35160c4782e48661f56d6b5ecc46c3c
Bintang Ramadhan <bintang4974.bintangramadhan4974@gmail.com>
2023-01-23 14:01:20 +0700

    fixing payment to dashboard admin

--- midtrans/examples/notification-handler.php
+++ midtrans/examples/notification-handler.php
@@ -5,17 +5,18 @@
 
 namespace Midtrans;
 
+use mysqli;
+
 require_once dirname(__FILE__) . '/../Midtrans.php';
 Config::$isProduction = false;
-Config::$serverKey = '<your server key>';
+Config::$serverKey = 'SB-Mid-server-_NIUKVktNCJWaY1mHpFmw8gs';
 
 // non-relevant function only used for demo/example purpose
 printExampleWarningMessage();
 
 try {
     $notif = new Notification();
-}
-catch (\Exception $e) {
+} catch (\Exception $e) {
     exit($e->getMessage());
 }
 
@@ -24,6 +25,7 @@ $transaction = $notif->transaction_status;
 $type = $notif->payment_type;
 $order_id = $notif->order_id;
 $fraud = $notif->fraud_status;
+$transaction_id = $notif->transaction_id;
 
 if ($transaction == 'capture') {
     // For credit card transaction, we need to check whether transaction is challenge by FDS or not
@@ -31,20 +33,27 @@ if ($transaction == 'capture') {
         if ($fraud == 'challenge') {
             // TODO set payment status in merchant's database to 'Challenge by FDS'
             // TODO merchant should decide whether this transaction is authorized or not in MAP
-            echo "Transaction order_id: " . $order_id ." is challenged by FDS";
+            echo "Transaction order_id: " . $order_id . " is challenged by FDS";
         } else {
             // TODO set payment status in merchant's database to 'Success'
-            echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
+            echo "Transaction order_id: " . $order_id . " successfully captured using " . $type;
         }
     }
 } else if ($transaction == 'settlement') {
     // TODO set payment status in merchant's database to 'Settlement'
-    echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
+    include "../../admin/koneksi.php";
+    mysqli_query($koneksi, "UPDATE transaksi SET status_transaksi='3', 
+    transaction_id='$transaction_id' WHERE order_id='$order_id'");
+    echo "Transaction order_id: " . $order_id . " successfully transfered using " . $type;
 } else if ($transaction == 'pending') {
     // TODO set payment status in merchant's database to 'Pending'
+    include "../../admin/koneksi.php";
+    mysqli_query($koneksi, "UPDATE transaksi SET status_transaksi='2'");
     echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
 } else if ($transaction == 'deny') {
     // TODO set payment status in merchant's database to 'Denied'
+    include "../../admin/koneksi.php";
+    mysqli_query($koneksi, "UPDATE transaksi SET status_transaksi='1'");
     echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
 } else if ($transaction == 'expire') {
     // TODO set payment status in merchant's database to 'expire'
@@ -54,11 +63,12 @@ if ($transaction == 'capture') {
     echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
 }
 
-function printExampleWarningMessage() {
+function printExampleWarningMessage()
+{
     if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         echo 'Notification-handler are not meant to be opened via browser / GET HTTP method. It is used to handle Midtrans HTTP POST notification / webhook.';
     }
-    if (strpos(Config::$serverKey, 'your ') != false ) {
+    if (strpos(Config::$serverKey, 'your ') != false) {
         echo "<code>";
         echo "<h4>Please set your server key from sandbox</h4>";
         echo "In file: " . __FILE__;
@@ -66,5 +76,5 @@ function printExampleWarningMessage() {
         echo "<br>";
         echo htmlspecialchars('Config::$serverKey = \'<your server key>\';');
         die();
-    }   
+    }
 }
